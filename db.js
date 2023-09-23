9e8b2f4427d97499ff9e1bdad0e9466689a026b1
surya-10 <sp659151@gmail.com>
2023-09-23 22:16:53 +0530

    done

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-let str = "mongodb+srv://forgot:forgot@cluster0.zjxmjix.mongodb.net/?retryWrites=true&w=majority";
+let mongoStr = "mongodb+srv://forgot:forgot@cluster0.zjxmjix.mongodb.net/?retryWrites=true&w=majority" 
 export async function dbConnection(){
-    let client = new MongoClient(str);
+    let client = new MongoClient(mongoStr);
     await client.connect();
     console.log("DB connected");
     return client;
