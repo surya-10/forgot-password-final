befff3ddd0c6b800ee0ab01197d0c149b6ef33a9
surya-10 <sp659151@gmail.com>
2023-09-23 21:44:52 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { client } from "./db.js";
+// import { client } from "./db.js";
 import dotenv from "dotenv";
 dotenv.config();
 // import { userRouter } from "./routes/route.js";
@@ -9,8 +9,9 @@ import cors from "cors";
 
 
 let app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
 // app.use("/", userRouter);
 
 let port = 9001;
